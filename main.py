80aedf1d852afce6455b45e57fb446ec04224644
Beraed <60759315+Beraed@users.noreply.github.com>
2020-02-24 04:55:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+from copy import deepcopy
 
 
 # FUNCTIONS
@@ -65,13 +66,14 @@ def count_neighbours(matrix):
 
 
 def flood_fill(pos_x, pos_y, last_cell):
-    global value_matrix, state_matrix
-    if state_matrix[pos_x][pos_y] != "H" or (value_matrix[pos_x][pos_y] != 0 and last_cell != 0):
+    global value_matrix, state_matrix, matrix_lines, matrix_columns
+
+    if state_matrix[pos_y][pos_x] != "H" or (value_matrix[pos_y][pos_x] != 0 and last_cell != 0):
         return None
     else:
-        state_matrix[pos_x][pos_y] = "S"
+        state_matrix[pos_y][pos_x] = "S"
 
-    last_cell = value_matrix[pos_x][pos_y]
+    last_cell = value_matrix[pos_y][pos_x]
 
     if pos_x + 1 != matrix_columns:
         flood_fill(pos_x + 1, pos_y, last_cell)
@@ -101,6 +103,7 @@ def win_check():
             if isinstance(value_matrix[i][j], int) and state_matrix[i][j] != "S":
                 return False
     return True
+
 
 # TODO SOLVE CASES
 
@@ -139,7 +142,6 @@ def solve_case_1():
                                                 state_matrix[i + k][j + l] = "F"
                                                 ok = True
 
-
     return ok
 
 
@@ -172,18 +174,76 @@ def solve_case_2():
                                     if j + l >= 0 and j + l < matrix_columns:
                                         if k != 0 or l != 0:
                                             if state_matrix[i + k][j + l] == "H" and value_matrix[i + k][j + l] == 0:
-                                                flood_fill(i + k, j + l, 0)
+                                                flood_fill(j + l, i + k, 0)
                                                 ok = True
                                             elif state_matrix[i + k][j + l] == "H":
                                                 state_matrix[i + k][j + l] = "S"
                                                 ok = True
-
 
     return ok
 
 
 def solve_case_3():
     global value_matrix, state_matrix, matrix_lines, matrix_columns, choice_x, choice_y
+
+    value_matrix_copy = deepcopy(value_matrix)
+    hidden_matrix = [[0 for j in range(matrix_columns)] for i in range(matrix_lines)]
+    probability_matrix = [[0 for j in range(matrix_columns)] for i in range(matrix_lines)]
+    divide_for_average_matrix = [[0 for j in range(matrix_columns)] for i in range(matrix_lines)]
+
+    # SCAD FLAGS SI NUMAR HIDDEN
+
+    for i in range(matrix_lines):
+        for j in range(matrix_columns):
+            if isinstance(value_matrix_copy[i][j], int) and state_matrix[i][j] == "S":
+                if value_matrix_copy[i][j] > 0:
+
+                    for k in range(-1, 2):
+                        for l in range(-1, 2):
+                            if i + k >= 0 and i + k < matrix_lines:
+                                if j + l >= 0 and j + l < matrix_columns:
+                                    if k != 0 or l != 0:
+                                        if state_matrix[i + k][j + l] == "F":
+                                            value_matrix_copy[i][j] -= 1
+                                        if state_matrix[i + k][j + l] == "H":
+                                            hidden_matrix[i][j] += 1
+
+    # ADAUG PROBABILITATILE INTR-O MATRICE
+
+    for i in range(matrix_lines):
+        for j in range(matrix_columns):
+            if isinstance(value_matrix_copy[i][j], int) and state_matrix[i][j] == "S":
+                if value_matrix_copy[i][j] > 0:
+
+                    for k in range(-1, 2):
+                        for l in range(-1, 2):
+                            if i + k >= 0 and i + k < matrix_lines:
+                                if j + l >= 0 and j + l < matrix_columns:
+                                    if k != 0 or l != 0:
+                                        if state_matrix[i + k][j + l] == "H":
+                                            probability_matrix[i + k][j + l] = probability_matrix[i + k][j + l] + \
+                                                                               value_matrix_copy[i][j] / \
+                                                                               hidden_matrix[i][j]
+                                            divide_for_average_matrix[i + k][j + l] += 1
+
+    # CALCULEZ PROBABILITATEA FINALA
+
+    for i in range(matrix_lines):
+        for j in range(matrix_columns):
+            if divide_for_average_matrix[i][j] != 0:
+                probability_matrix[i][j] = probability_matrix[i][j] / divide_for_average_matrix[i][j]
+
+    # PROBABILITATE MINIMA
+    minimum_value = 2
+    choice_x = -1
+    choice_y = -1
+
+    for i in range(matrix_lines):
+        for j in range(matrix_columns):
+            if probability_matrix[i][j] < minimum_value and probability_matrix[i][j] != 0:
+                minimum_value = probability_matrix[i][j]
+                choice_y = i
+                choice_x = j
 
     return True
 
@@ -197,19 +257,26 @@ pygame.display.set_icon(icon)
 
 # MAIN GAME LOOP
 
+solve_count = 0
+solve_win_count = 0
+
+begginer_difficulty = [8 ,8 ,10]
+intermediate_difficulty = [16, 16, 40]
+expert_difficulty = [16, 30, 99]
+
 game_state = "new_game"
 while game_state != "quit":
 
     if game_state == "new_game":
-        value_matrix = create_matrix(8, 8, 8)
+        value_matrix = create_matrix(expert_difficulty[0], expert_difficulty[1], expert_difficulty[2])
         state_matrix = [["H" for j in range(len(value_matrix[i]))] for i in range(len(value_matrix))]
-
-        print_matrix(value_matrix)
 
         matrix_lines = len(value_matrix)
         matrix_columns = len(value_matrix[0])
 
+        print("")
         print("-" * (matrix_columns * 2 - 1))
+        print_matrix(value_matrix)
 
         header = 64
         first_click = True
@@ -246,7 +313,7 @@ while game_state != "quit":
 
                         if state_matrix[line][column] == "H":
                             if value_matrix[line][column] == 0:
-                                flood_fill(line, column, 0)
+                                flood_fill(column, line, 0)
 
                             if value_matrix[line][column] == "B":
                                 value_matrix[line][column] = "X"
@@ -382,6 +449,9 @@ while game_state != "quit":
 
     if game_state == "solve":
 
+        solve_count += 1
+        print("Solve steps: ", end="")
+
         for i in range(matrix_lines):
             for j in range(matrix_columns):
                 if state_matrix[i][j] == "F":
@@ -408,38 +478,62 @@ while game_state != "quit":
                         if mouse_position_x > matrix_columns * 16 - 26 and mouse_position_x < matrix_columns * 16 + 26 and mouse_position_y > 6 and mouse_position_y < 58:
                             game_state = "new_game"
 
+                    if event.button == 3:
+                        if mouse_position_x > matrix_columns * 16 - 26 and mouse_position_x < matrix_columns * 16 + 26 and mouse_position_y > 6 and mouse_position_y < 58:
+                            running = True
+                            while running:
+
+                                for event in pygame.event.get():
+                                    if event.type == pygame.QUIT:
+                                        game_state = "quit"
+                                        running = False
+
+                                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                                        mouse_position = pygame.mouse.get_pos()
+                                        mouse_position_x = mouse_position[0]
+                                        mouse_position_y = mouse_position[1]
+
+                                        if event.button == 1:
+                                            if mouse_position_x > matrix_columns * 16 - 26 and mouse_position_x < matrix_columns * 16 + 26 and mouse_position_y > 6 and mouse_position_y < 58:
+                                                game_state = "new_game"
+                                                running = False
+
+                                        if event.button == 3:
+                                            if mouse_position_x > matrix_columns * 16 - 26 and mouse_position_x < matrix_columns * 16 + 26 and mouse_position_y > 6 and mouse_position_y < 58:
+                                                running = False
+
             # TODO ALGORITHM
             # CHOICE
 
             if first_click:
-                choice_x = random.randint(1,matrix_columns-2)
-                choice_y = random.randint(1,matrix_lines-2)
+                print("F", end=" ")
+                choice_x = random.randint(1, matrix_columns - 2)
+                choice_y = random.randint(1, matrix_lines - 2)
 
             result_1 = solve_case_1()
             result_2 = solve_case_2()
 
             if result_1:
-                print("1")
+                print("1", end=" ")
             if result_2:
-                print("2")
+                print("2", end=" ")
 
             result_3 = False
             if result_1 == False and result_2 == False and first_click == False:
+                print("3", end=" ")
                 result_3 = solve_case_3()
-                print("3")
 
             # REPERCURSION
 
             if first_click or result_3:
-                if state_matrix[choice_x][choice_y] == "H":
-                    if value_matrix[choice_x][choice_y] == 0:
+                if state_matrix[choice_y][choice_x] == "H":
+                    if value_matrix[choice_y][choice_x] == 0:
                         flood_fill(choice_x, choice_y, 0)
-                    elif isinstance(value_matrix[choice_x][choice_y], int):
-                        state_matrix[choice_x][choice_y] = "S"
-                    elif value_matrix[choice_x][choice_y] == "B":
-                        value_matrix[choice_x][choice_y] = "X"
+                    elif isinstance(value_matrix[choice_y][choice_x], int):
+                        state_matrix[choice_y][choice_x] = "S"
+                    elif value_matrix[choice_y][choice_x] == "B":
+                        value_matrix[choice_y][choice_x] = "X"
                         game_state = "lose"
-
 
             first_click = False
             if win_check():
@@ -467,5 +561,13 @@ while game_state != "quit":
 
                     screen.blit(tile_image, (j * 32, header + i * 32))
 
-            time.sleep(0.1)
+            # AFISARE STATISTICA
+            if game_state == "win":
+                solve_win_count += 1
+                print("\nAlgoritmul a castigat! Rata de succes este ", solve_win_count, " din ", solve_count, ".",sep="")
+
+            elif game_state == "lose":
+                print("\nAlgoritmul a pierdut! Rata de succes este ", solve_win_count, " din ", solve_count, ".",sep="")
+
+            time.sleep(0.2)
             pygame.display.update()
